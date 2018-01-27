6b5d9299f94d338ed5c9bdb328dd511a6d5b9a00
knit tkg <ktakagi@nttpc.co.jp>
2018-01-27 08:09:50 +0000

    Copied Glyphicons to app/assets/fonts

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Sample
     # https://gascar-shunt.tumblr.com/post/65236279971/rails-4%E3%81%A7bootstrap-3-glyphicon%E5%95%8F%E9%A1%8C%E3%82%82%E8%A7%A3%E6%B1%BA
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
+    # http://d.hatena.ne.jp/gakeno_ueno_horyo/20150118/1421560645
+    config.assets.paths << "#{Rails}/app/assets/fonts"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
